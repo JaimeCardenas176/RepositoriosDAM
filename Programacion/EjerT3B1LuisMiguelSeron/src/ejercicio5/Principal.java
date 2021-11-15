ee4d574a2c063099553f745140a338340a909d44
Luis Miguel Serón Delgado <luis.miguel.seron@gmail.com>
2021-11-15 12:24:40 +0100

    calculadora en java

---
+++ Programacion/EjerT3B1LuisMiguelSeron/src/ejercicio5/Principal.java
@@ -0,0 +1,16 @@
+package ejercicio5;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// 5. Crear una clase con varios métodos que generen aleatoriamente distintos números o 
+		//resultados según las características del sorteo o para qué se vaya a usar, por ejemplo, 
+		//generar 1, x, 2 para una quiniela, generar par o impar para jugar a pares o nones, generar 
+		//1, 2 o 3 para jugar a los "chinos", del 1 al 49 para el sorteo de primitiva... Crear una 
+		//clase principal donde se prueben todos.
+		
+		
+		
+	}
+
+}
