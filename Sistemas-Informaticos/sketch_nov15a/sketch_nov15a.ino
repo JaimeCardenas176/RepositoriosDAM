ceb7d53ae9b6c323de94e44d12e0aae14bf7e4f6
Luis Miguel Serón Delgado <luis.miguel.seron@gmail.com>
2021-11-17 13:35:43 +0100

    arduino actualizado

--- Sistemas-Informaticos/sketch_nov15a/sketch_nov15a.ino
+++ Sistemas-Informaticos/sketch_nov15a/sketch_nov15a.ino
@@ -1,16 +1,27 @@
 // Práctica encender y apagar un LED a través de botón pulsador
 const int LED=13;
-const int BOTON=2;
+const int BOTON1=2;
+const int BOTON2=4;
 int val;
+int val2;
 void setup(){
 pinMode(LED,OUTPUT);
-pinMode(BOTON,INPUT);
+pinMode(BOTON1,INPUT);
+pinMode(BOTON2,INPUT);
 }
 void loop(){
-val=digitalRead(BOTON);
+val=digitalRead(BOTON1);
 if  (val==HIGH){
 digitalWrite(LED,HIGH);
+}else { digitalWrite(LED,LOW);
 }
-else { digitalWrite(LED,LOW);
+val2=digitalRead(BOTON2);
+if (val2==HIGH){
+  digitalWrite(LED,HIGH);
+  delay(150);
+  digitalWrite(LED,LOW);
+  delay(150);
+  }else {
+  digitalWrite(LED,LOW);
 }
 }
