fb3ca7a013de8942076a956e05c3566e5ec5d337
Luis Miguel Serón Delgado <luis.miguel.seron@gmail.com>
2021-11-19 12:10:02 +0100

    continuo el ejercicio 5

--- Programacion/EjerT3B1LuisMiguelSeron/src/ejercicio5/Principal.java
+++ Programacion/EjerT3B1LuisMiguelSeron/src/ejercicio5/Principal.java
@@ -2,6 +2,8 @@ package ejercicio5;
 
 import java.util.Iterator;
 
+import lectura.Leer;
+
 public class Principal {
 
 	public static void main(String[] args) {
@@ -10,22 +12,40 @@ public class Principal {
 		int j1 = aleatorio.chinos();
 		int j2 = aleatorio.chinos();
 		int j3 = aleatorio.chinos();
+		int totalj1;
+		int totalj2;
+		int totalj3;
 		
-		int totalj1= 5;
-		int totalj2= 7;
-		int totalj3= 3;
+		System.out.println("Bienvenido al juego de los chinos!");
+		System.out.println("Jugador 1 - Introduce un número del del 1 al 3");
+		totalj1=Leer.datoInt();
+
+		System.out.println("Jugador 2 - Introduce un número del del 1 al 3");
+		totalj2=Leer.datoInt();
 		
-		int total= j1+j2+j3;
+		System.out.println("Jugador 3 - Introduce un número del del 1 al 3");
+		totalj3=Leer.datoInt();
 		
-		System.out.println("El total de monedas que se han sacado es "+ total);
-		
-		for (int i = 0; i < 30; i++) {
-			System.out.println("c "+aleatorio.chinos());
+		if (totalj1==j1) {
+
+			System.out.println("El jugador uno ha ganado!");
+		} else {
+			System.out.println("El jugador uno ha perdido!");
 		}
-		for (int i = 0; i < 30; i++) {
-			System.out.println("quini "+aleatorio.quiniela());
+		if (totalj2==j2) {
+
+			System.out.println("El jugador dos ha ganado!");
+		} else {
+			System.out.println("El jugador dos ha perdido!");
+		}
+		if (totalj3==j3) {
+
+			System.out.println("El jugador tres ha ganado!");
+		} else {
+			System.out.println("El jugador tres ha perdido!");
 		}
 		
 	}
+	// Sigue sin estar acabado Jaime, que sé que estás leyéndome.
 
 }
