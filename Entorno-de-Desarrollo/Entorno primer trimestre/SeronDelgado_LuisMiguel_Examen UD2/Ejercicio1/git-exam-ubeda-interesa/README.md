b9c7988f2199616b9043eb968a45477d25623ed4
Luis Miguel Serón Delgado <luis.miguel.seron@gmail.com>
2021-11-29 14:03:36 +0100

    skrrr

---
+++ Entorno-de-Desarrollo/Entorno primer trimestre/SeronDelgado_LuisMiguel_Examen UD2/Ejercicio1/git-exam-ubeda-interesa/README.md
@@ -0,0 +1,2 @@
+# Ejercicio 1 - Web turística sobre Úbeda
+
--- Programacion/EjerT3B1LuisMiguelSeron/src/ejercicio5/Principal.java
+++ Programacion/EjerT3B1LuisMiguelSeron/src/ejercicio5/Principal.java
@@ -7,36 +7,33 @@ public class Principal {
 	public static void main(String[] args) {
 			
 		Suerte aleatorio = new Suerte();
-		int j1 = aleatorio.chinos();
-		int j2 = aleatorio.chinos();
-		int j3 = aleatorio.chinos();
-		int totalj1;
-		int totalj2;
-		int totalj3;
+		int j1, j2, j3;
+		int total= aleatorio.chinos();
+
 		
 		System.out.println("Bienvenido al juego de los chinos!");
 		System.out.println("Jugador 1 - Introduce un número del del 1 al 3");
-		totalj1=Leer.datoInt();
+		j1=Leer.datoInt();
 
 		System.out.println("Jugador 2 - Introduce un número del del 1 al 3");
-		totalj2=Leer.datoInt();
+		j2=Leer.datoInt();
 		
 		System.out.println("Jugador 3 - Introduce un número del del 1 al 3");
-		totalj3=Leer.datoInt();
+		j3=Leer.datoInt();
 		
-		if (totalj1==j1) {
+		if (total==j1) {
 
 			System.out.println("El jugador uno ha ganado!");
 		} else {
 			System.out.println("El jugador uno ha perdido!");
 		}
-		if (totalj2==j2) {
+		if (total==j2) {
 
 			System.out.println("El jugador dos ha ganado!");
 		} else {
 			System.out.println("El jugador dos ha perdido!");
 		}
-		if (totalj3==j3) {
+		if (total==j3) {
 
 			System.out.println("El jugador tres ha ganado!");
 		} else {
