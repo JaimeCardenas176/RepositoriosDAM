ce3a9dce06e3c6493ce0a8e6db8dc60e96e2dc8a
Luis Miguel Serón Delgado <luis.miguel.seron@gmail.com>
2021-12-09 14:25:25 +0100

    skrr

--- Programacion/EjerT3B1LuisMiguelSeron/src/ejercicio2/Principal.java
+++ Programacion/EjerT3B1LuisMiguelSeron/src/ejercicio2/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(100<0);
+		System.out.println(100>0);
 		Operaciones operacion = new Operaciones();
 		
 		System.out.println(operacion.esPar(23));
