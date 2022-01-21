631211284550f5090a2194b21ffb857d21b419c8
Luis Miguel Serón Delgado <luis.miguel.seron@gmail.com>
2022-01-21 12:39:50 +0100

    skrr

--- Programacion/ExtraT3/src/ejercicio04/Principal.java
+++ Programacion/ExtraT3/src/ejercicio04/Principal.java
@@ -4,7 +4,10 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-
+	Cliente c = new Cliente("53351608", "Luismi", "Seron", 70.00, 1.75, false );	
+	
+	System.out.println(c.calcularIMC());
+	
 	}
 
 }
